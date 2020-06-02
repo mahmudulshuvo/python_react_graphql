958b07cd759653573437c339f8dd6b77c48c0764
ShuvoHassan <shuvo.hassan@hotmail.com>
2020-06-02 22:07:33 +0600

    Add react-client and connect with backend

---
+++ react-tracks-client/src/Root.js
@@ -0,0 +1,26 @@
+import React from "react";
+import withRoot from "./withRoot";
+import {Query} from 'react-apollo';
+import {gql} from 'apollo-boost';
+
+const Root=() => (<Query query={GET_TRACK_LIST}>
+    {({data, loading, error}) => {
+        if (loading) return <div>Loading</div>
+        if (error) return <div>Error</div>
+
+        return <div>{JSON.stringify(data)}</div>
+    }}
+</Query>)
+
+const GET_TRACK_LIST=gql`
+{
+  tracks{
+    id
+    title
+    description
+    url
+  }
+}
+`
+
+export default withRoot(Root);
